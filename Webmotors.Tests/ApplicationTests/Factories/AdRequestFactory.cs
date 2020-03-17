5afdfe08608263b3b3ec8e0629df086485f6e769
Loreto <loretorafa@gmail.com>
2020-03-17 20:01:54 -0300

    Add project files.

---
+++ Webmotors.Tests/ApplicationTests/Factories/AdRequestFactory.cs
@@ -0,0 +1,43 @@
+﻿using Webmotors.Application.Requests;
+using Webmotors.Domain.Interfaces.Requests;
+
+namespace Webmotors.Tests.ApplicationTests.Factories
+{
+    public class AdRequestFactory
+    {
+        public IAdRequest GetAdRequest(bool valid = true)
+        {
+            return valid ? ValidRequest : InvalidRequest;
+        }
+
+        private AdRequest ValidRequest 
+        {
+            get
+            {
+                var request = new AdRequest();
+                request.Make = "VOLKSWAGEN";
+                request.Model = "FUSCA";
+                request.Version = "1.3 8V GASOLINA 2P MANUAL";
+                request.Year = 1981;
+                request.Mileage = 150000;
+                request.Note = "fusca muito bonito";
+
+                return request;
+            }
+        }
+
+        private AdRequest InvalidRequest
+        {
+            get
+            {
+                var request = new AdRequest();
+                request.Make = "MARCA MUITO LONGA QUE NÃO VAI CABER NO LIMITE DE QUARENTA E CINCO CARACTERES PROPOSTO NESTE DESAFIO";
+                request.Model = "";
+                request.Version = null;
+                //Ano, Km e Obs omitidos
+
+                return request;
+            }
+        }
+    }
+}
